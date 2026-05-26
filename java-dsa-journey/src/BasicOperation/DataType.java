5864e8442fb2adb800996707b5ef053a49098ad7
Prince kumar <118436358+princeep@users.noreply.github.com>
2026-05-27 01:21:18 +0530

    Added Java data types examples

---
+++ java-dsa-journey/src/BasicOperation/DataType.java
@@ -0,0 +1,35 @@
+package BasicOperation;
+
+public class DataType {
+    public static void main(String[] args) {
+
+        // Integer Data Types
+        byte byteValue = 127;
+        short shortValue = 32000;
+        int intValue = 100000;
+        long longValue = 999999999L;
+
+        // Decimal Data Types
+        float floatValue = 10.5f;
+        double doubleValue = 99.99;
+
+        // Character Data Type
+        char charValue = 'P';
+
+        // Boolean Data Type
+        boolean booleanValue = true;
+
+        // Print Values
+        System.out.println("byte: " + byteValue);
+        System.out.println("short: " + shortValue);
+        System.out.println("int: " + intValue);
+        System.out.println("long: " + longValue);
+
+        System.out.println("float: " + floatValue);
+        System.out.println("double: " + doubleValue);
+
+        System.out.println("char: " + charValue);
+
+        System.out.println("boolean: " + booleanValue);
+    }
+}
