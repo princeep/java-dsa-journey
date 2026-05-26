05874e184dbd86a56311cd2b2def7f62ebb19fb1
Prince kumar <118436358+princeep@users.noreply.github.com>
2026-05-26 23:04:55 +0530

    added single line and multiple line comment

--- java-dsa-journey/src/BasicOperation/FirstProgram.java
+++ java-dsa-journey/src/BasicOperation/FirstProgram.java
@@ -3,5 +3,13 @@ package BasicOperation;
 public class FirstProgram {
     public static void main(String[] args){
         System.out.println("Hello");
+
+//        System.out.println("this is first comment");
+
+        /*
+        System.out.println("this is first program");
+        System.out.println("this multiple line comment");
+        
+         */
     }
 }
