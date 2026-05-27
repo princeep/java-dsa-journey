e6566c4574f68e17dd8114af66e59112584d1f39
Prince kumar <118436358+princeep@users.noreply.github.com>
2026-05-27 23:23:53 +0530

    Implement arithmetic operations

---
+++ java-dsa-journey/src/BasicOperation/Operators.java
@@ -0,0 +1,21 @@
+package BasicOperation;
+
+public class Operators {
+    public static void main(String[] args){
+//    Arithmetic Operators
+    int solvedThisWeek = 25;
+    int solvedLastWeek = 15;
+
+    int total = solvedThisWeek + solvedLastWeek;
+    int difference = solvedThisWeek - solvedLastWeek;
+    int projected = solvedThisWeek * 4;
+    int average = solvedThisWeek / 7;
+    int remainder = solvedThisWeek % 7;
+
+        System.out.println(total);
+        System.out.println(difference);
+        System.out.println(projected);
+        System.out.println(average);
+        System.out.println(remainder);
+}
+}
