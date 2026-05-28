a22c99aff8eed74f8a05b71afe12e15d65c3be82
Prince kumar <118436358+princeep@users.noreply.github.com>
2026-05-28 23:57:34 +0530

    Implement if else statement

---
+++ java-dsa-journey/src/BasicOperation/ConditionalStatement.java
@@ -0,0 +1,19 @@
+package BasicOperation;
+
+
+public class ConditionalStatement {
+    public static void main(String[] args) {
+        int score = 50;
+//        if(score>=30){
+//            System.out.println("Pass");
+//        }
+//
+        if(score>50){
+            System.out.println("Pass");
+        }
+        else{
+            System.out.println("Fail");
+        }
+
+    }
+}
