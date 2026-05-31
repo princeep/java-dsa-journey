d2936d99a1f8d101b55c18887a75d27571a18392
Prince kumar <118436358+princeep@users.noreply.github.com>
2026-05-31 22:05:34 +0530

    add login method using username and password validation

--- java-dsa-journey/src/BasicOperation/Methods.java
+++ java-dsa-journey/src/BasicOperation/Methods.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class Methods {
 //    create a method for login and signup
-    void signup(){
         Scanner sc = new Scanner(System.in);
+    void signup(){
+        System.out.println("Signup page");
         System.out.println("Enter username");
         String username = sc.next();
         System.out.println("Enter gmail");
@@ -29,8 +30,25 @@ public class Methods {
         }
 
     }
+    void login(){
+        System.out.println("Login page");
+        System.out.println("Enter gmail");
+        String password = sc.next();
+        System.out.println("Enter confirm Password");
+        String confirmPassword = sc.next();
+        if(password.isEmpty() || password.length()<8){
+            System.out.println("password should be 8 char");
+        }
+        else if(!password.equals(confirmPassword)){
+            System.out.println("confirm Password missing");
+        }
+        else {
+            System.out.println("Login successfully");
+        }
+    }
     public static void main(String[] args){
         Methods methodObject = new Methods();
         methodObject.signup();
+        methodObject.login();
     }
 }
