5b7b61873928226b8acf68a107b1d054a756e5a0
Prince kumar <118436358+princeep@users.noreply.github.com>
2026-06-01 23:54:05 +0530

    Add min and max element logic in Arrays

--- java-dsa-journey/src/BasicOperation/Arrays.java
+++ java-dsa-journey/src/BasicOperation/Arrays.java
@@ -5,11 +5,29 @@ public class Arrays {
     public static void main(String[] args) {
 
         //    Problem 1: Sum of Elements
-        int[] arr = {2, 4, 6, 8, 10};
+        int[] arr = {2, 4, 60, 8, 10};
         int sum = 0;
         for (int i = 0; i < arr.length;i++) {
             sum += arr[i];
         }
             System.out.println("Sum of Array" + sum);
+
+//        Problem 2: Maximum and Minimum Element
+        int min = arr[0];
+        int max = arr[0];
+
+        for(int i=0;i<arr.length;i++){
+            if(max>arr[i]){
+                max = arr[i];
+            }
+            if(min<arr[i]){
+                min = arr[i];
+            }
+        }
+        System.out.println("Max Element"+ max);
+        System.out.println("Min Element"+ min);
+
     }
-}
+
+    }
+
