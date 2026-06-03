50a6753b1bcf2f3a5c67fd6287e57ba18d6ac939
Prince kumar <118436358+princeep@users.noreply.github.com>
2026-06-03 23:59:16 +0530

    Add Java String basics program

---
+++ java-dsa-journey/src/BasicOperation/StringExample.java
@@ -0,0 +1,9 @@
+package BasicOperation;
+
+public class StringExample {
+    public static void main(String[] args) {
+
+        String str = "Prince";
+        System.out.println(str.length());
+    }
+}
