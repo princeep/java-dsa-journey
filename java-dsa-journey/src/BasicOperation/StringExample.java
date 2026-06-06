91a67361222cfbde0f091a9790873bd8a8fa282c
Prince kumar <118436358+princeep@users.noreply.github.com>
2026-06-06 23:20:41 +0530

    Add string reversal and palindrome check logic

--- java-dsa-journey/src/BasicOperation/StringExample.java
+++ java-dsa-journey/src/BasicOperation/StringExample.java
@@ -14,21 +14,38 @@ public class StringExample {
 
 //        Count Length Without length()
         int count = 0;
-        for(int i=0;i<str.length();i++){
+        for (int i = 0; i < str.length(); i++) {
             count++;
         }
-        System.out.println("number of character"+count);
+        System.out.println("number of character" + count);
 
 //        Count Vowels
-        for(int i=0;i<str.length();i++){
+        for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
-            if(ch=='a' || ch=='A' || ch=='e' || ch=='E' || ch=='i' || ch=='I' || ch=='o' || ch=='O' || ch=='u' || ch=='U'){
+            if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch == 'o' || ch == 'O' || ch == 'u' || ch == 'U') {
                 System.out.println(ch + " is vowel");
-            }
-            else {
+            } else {
                 System.out.println(ch + "is consonunt");
             }
         }
-        
+//        Reverse a String
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+        System.out.println("Reverse string is " + rev);
+
+//        Check Palindrome
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        if (str.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+
+        }
     }
 }
